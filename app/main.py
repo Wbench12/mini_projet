a49e0c7591009040440b43842d2db4dc21c3c48c
Benhammadi Lokmane <l.benhammadi@esi-sba.dz>
2025-12-13 13:43:29 +0100

    API READY

--- app/main.py
+++ app/main.py
@@ -70,14 +70,20 @@ app = FastAPI(
 )
 
 # Add CORS middleware
-if settings.cors_origins:
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=settings.cors_origins,
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:5173", 
+        "http://localhost:5174",
+        "http://127.0.0.1:3000",
+        "http://127.0.0.1:5173",
+        "http://127.0.0.1:5174",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Add security middleware
 app.add_middleware(
